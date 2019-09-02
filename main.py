33ae892c3ecede2a0714dfd146e1bb86539136dd
Gabriel Carvalho <gabriel315df@gmail.com>
2019-09-02 02:13:08 -0300

    Added initial menu

--- main.py
+++ main.py
@@ -1,25 +1,154 @@
+import os
 import sys
-sys.path.append('./src')
-import discipline_search as ds
 import json
+from src import discipline_search as ds
 
 
-with open('data/d.json') as f:
-  disciplines = json.load(f)
+def main():
+    os.system('cls||clear')
+    print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n')
+    print('Estrutura de Dados 2 - Métodos de Busca\n')
+    print('==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n')
+    print('Selecione uma opção: ' + '\n')
+    print('[1] - Buscar Matérias')
+    print('[2] - Buscar em um Array')
+    print('[3] - Sair')
+    print('')
+    print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n' + '\033[0m')
+    option = int(input('>>>'))
 
-print("Insiria o código da disciplina: ")
-code = int(input())
+    while(option not in [1, 2, 3]):
+        option = int(input("Insira uma opção válida: "))
 
-i = ds.binary_search(disciplines, code)
-print(i['code'], '-', i['name'])
+    if(option == 1):
+        discipline_menu()
+    elif(option == 2):
+        pass
+    else:
+        sys.exit()
 
-if(i['requirements']):
-  print("Pré-Requisitos: ")
-  for requirements in i['requirements']:
-    requirement = requirements.split(',')
-    for req in requirement:
-      r = ds.binary_search(disciplines, int(req))
-      print('\t', r['code'], '-', r['name'])
 
-else:
-  print("Matéria sem pré-requisitos")
+def discipline_menu():
+
+    os.system('cls||clear')
+
+    with open('data/disciplines.json') as f:
+        disciplines = json.load(f)
+
+    print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n')
+    print('Estrutura de Dados 2 - Métodos de Busca\n')
+    print('==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n')
+    print('Selecione uma opção de busca: ' + '\n')
+    print('[1] - Busca sequêncial')
+    print('[2] - Busca binária')
+    print('[3] - Busca interpolação')
+    print('[4] - Buscar indexada')
+    print('[5] - Sair')
+    print('')
+    print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n' + '\033[0m')
+    option = int(input('>>>'))
+
+    while(option not in [1, 2, 3, 4, 5]):
+        option = int(input("Insira uma opção válida: "))
+    
+    os.system('cls||clear')
+    print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n')
+    print("Insiria o código da disciplina: ")
+    code = int(input('>>>'))
+    print('==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n')
+
+    if (option == 1):
+
+        i = ds.linear_search(disciplines, code)
+        if(i):
+            print(i['code'], '-', i['name'])
+            if(i['requirements']):
+                print("Pré-Requisitos: ")
+                for requirements in i['requirements']:
+                    requirement = requirements.split(',')
+                    for req in requirement:
+                        r = ds.linear_search(disciplines, int(req))
+                        if(r):
+                            print('\t', r['code'], '-', r['name'])
+
+            else:
+                print("Matéria sem pré-requisitos\n")
+        
+        else:
+            print("Matéria não encontrada\n")
+        
+        print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n' + '\033[0m')
+    
+
+    elif (option == 2):
+
+        i = ds.binary_search(disciplines, code)
+        if(i):
+            print(i['code'], '-', i['name'])
+            if(i['requirements']):
+                print("Pré-Requisitos: ")
+                for requirements in i['requirements']:
+                    requirement = requirements.split(',')
+                    for req in requirement:
+                        r = ds.binary_search(disciplines, int(req))
+                        if(r):
+                            print('\t', r['code'], '-', r['name'])
+
+            else:
+                print("Matéria sem pré-requisitos\n")
+        
+        else:
+            print("Matéria não encontrada\n")
+        
+        print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n' + '\033[0m')
+
+    elif (option == 3):
+
+        i = ds.interpolation_search(disciplines, code)
+        if(i):
+            print(i['code'], '-', i['name'])
+            if(i['requirements']):
+                print("Pré-Requisitos: ")
+                for requirements in i['requirements']:
+                    requirement = requirements.split(',')
+                    for req in requirement:
+                        r = ds.interpolation_search(disciplines, int(req))
+                        if(r):
+                            print('\t', r['code'], '-', r['name'])
+
+            else:
+                print("Matéria sem pré-requisitos\n")
+        
+        else:
+            print("Matéria não encontrada\n")
+        
+        print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n' + '\033[0m')
+
+    elif (option == 4):
+
+        a = ds.create_index(disciplines)
+        i = ds.indexed_search(a, disciplines, code)
+
+        if(i):
+            print(i['code'], '-', i['name'])
+            if(i['requirements']):
+                print("Pré-Requisitos: ")
+                for requirements in i['requirements']:
+                    requirement = requirements.split(',')
+                    for req in requirement:
+                        r = ds.indexed_search(a, disciplines, int(req))
+                        if(r):
+                            print('\t', r['code'], '-', r['name'])
+            else:
+                print("Matéria sem pré-requisitos\n")
+        else:
+            print("Matéria não encontrada\n")
+        
+        print('\033[1m'+'==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==\n' + '\033[0m')
+        
+    else:
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
--- src/discipline_search.py
+++ src/discipline_search.py
@@ -41,8 +41,7 @@ def interpolation_search(array, num):
   low = 0
   high = len(array) - 1
   while (low <= high and num >= int(array[low]['code']) and num <= int(array[high]['code'])):
-    mid = low + int(
-        ((float(high - low) / (int(array[high]['code']) - int(array[low]['code']))) * (num - int(array[low]['code']))))
+    mid = low + int(((float(high - low) / (int(array[high]['code']) - int(array[low]['code']))) * (num - int(array[low]['code']))))
     if (int(array[mid]['code']) == num):
         return array[mid]
     if (int(array[mid]['code']) < num):
@@ -56,15 +55,15 @@ def indexed_search(idx, array, num):
   low = 0
   for i in range (0, len(idx)):
     if(num == idx[i][0]):
-      return idx[i][1]
+      return array[idx[i][1]]
     elif(idx[i][0] > num):
       for x in range(low, idx[i][1]):
         if(num == int(array[x]['code'])):
-          return x
+          return array[x]
     elif(num > idx[-1][0]):
       for x in range(idx[-1][1], len(array)):
         if(num == int(array[x]['code'])):
-          return x
+          return array[x]
     low = idx[i][1]
 
   return False
